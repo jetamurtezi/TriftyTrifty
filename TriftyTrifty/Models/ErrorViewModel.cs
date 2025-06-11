1407298a880a82a19df86fc30600c921e632662a
jetamurtezi <jetamurtezi98@gmail.com>
2025-06-11 09:52:18 +0200

    initial commit

---
+++ TriftyTrifty/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TriftyTrifty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
