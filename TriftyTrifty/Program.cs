a6e5cd197c28db3bdfdf90b9ea099b89fe7dddca
jetamurtezi <jetamurtezi98@gmail.com>
2025-06-11 10:14:28 +0200

    update program.cs

--- TriftyTrifty/Program.cs
+++ TriftyTrifty/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TriftyTrifty.Data;
+
 namespace TriftyTrifty
 {
     public class Program
@@ -8,6 +11,7 @@ namespace TriftyTrifty
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
